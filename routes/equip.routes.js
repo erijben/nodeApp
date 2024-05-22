aba41ee39ec5973077789e87e36fbefbd154c1d6
erijben <129642847+erijben@users.noreply.github.com>
2024-05-23 00:22:21 +0100

    hellogjhjgj

--- routes/equip.routes.js
+++ routes/equip.routes.js
@@ -9,7 +9,26 @@ router.post("/add", (req, res) => {
   equipCtrl.apiCreateequip(req, res);});
    
 router.get("/equip/:id", equipCtrl.apiGetequipById);
-router.put("/equip/:id", equipCtrl.apiUpdateequip);
+router.put('/equip/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const updateData = req.body;
+
+    console.log('Requête PUT reçue pour mettre à jour l\'équipement:', id);
+    console.log('Données de mise à jour:', updateData);
+
+    const updatedEquip = await Equip.findByIdAndUpdate(id, updateData, { new: true });
+    if (!updatedEquip) {
+      return res.status(404).json({ message: 'Équipement non trouvé' });
+    }
+
+    res.json(updatedEquip);
+  } catch (error) {
+    console.error('Erreur lors de la mise à jour de l\'équipement:', error);
+    res.status(500).json({ message: 'Erreur serveur' });
+  }
+});
+
 router.delete('/:id', equipCtrl.apiDeleteequip);
 router.get("/find/:rfid", equipCtrl.apiGetEquipByRfid);
 module.exports =  router;
