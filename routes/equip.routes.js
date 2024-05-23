366047b5c67f8092644b8f0430338c77eb3b545f
erijben <129642847+erijben@users.noreply.github.com>
2024-05-23 14:13:46 +0100

    nshalah tekhdem amanhhhhhhhhhh

--- routes/equip.routes.js
+++ routes/equip.routes.js
@@ -1,27 +1,23 @@
-const  express =  require("express");
+const express = require("express");
 const router = express.Router();
 const equipCtrl = require("../controllers/equip.controller");
 const Equip = require('../models/equip');
 
-
 router.get("/", equipCtrl.apiGetAllequips);
 router.post("/add", (req, res) => {
   console.log("POST request to add");
-  equipCtrl.apiCreateequip(req, res);});
-   
+  equipCtrl.apiCreateequip(req, res);
+});
 router.get("/equip/:id", equipCtrl.apiGetequipById);
 router.put("/equip/:id", async (req, res) => {
   try {
     const { id } = req.params;
     const updateData = req.body;
-
     console.log(`Requête de mise à jour reçue pour ${id} avec les données: `, updateData);
-
     const updatedEquip = await Equip.findByIdAndUpdate(id, updateData, { new: true }).populate("ConnecteA");
     if (!updatedEquip) {
       return res.status(404).json({ message: "Équipement non trouvé" });
     }
-
     console.log(`Équipement mis à jour: ${updatedEquip}`);
     res.json(updatedEquip);
   } catch (error) {
@@ -29,7 +25,6 @@ router.put("/equip/:id", async (req, res) => {
     res.status(500).json({ message: "Erreur serveur" });
   }
 });
-
 router.delete('/:id', equipCtrl.apiDeleteequip);
 router.get("/find/:rfid", equipCtrl.apiGetEquipByRfid);
-module.exports =  router;
+module.exports = router;
