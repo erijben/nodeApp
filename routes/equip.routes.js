e739cf6641223081b2757e7df7fc51af94aeb0fc
erijben <129642847+erijben@users.noreply.github.com>
2024-05-23 14:22:05 +0100

    nshalah tekhdem amanhhhhhhhhhhh

--- routes/equip.routes.js
+++ routes/equip.routes.js
@@ -9,16 +9,16 @@ router.post("/add", (req, res) => {
   equipCtrl.apiCreateequip(req, res);
 });
 router.get("/equip/:id", equipCtrl.apiGetequipById);
-router.put("/equip/:id", async (req, res) => {
+router.put('/equip/:id', async (req, res) => {
   try {
     const { id } = req.params;
     const updateData = req.body;
-    console.log(`Requête de mise à jour reçue pour ${id} avec les données: `, updateData);
+
     const updatedEquip = await Equip.findByIdAndUpdate(id, updateData, { new: true }).populate("ConnecteA");
     if (!updatedEquip) {
       return res.status(404).json({ message: "Équipement non trouvé" });
     }
-    console.log(`Équipement mis à jour: ${updatedEquip}`);
+
     res.json(updatedEquip);
   } catch (error) {
     console.error("Erreur lors de la mise à jour de l'équipement:", error);
